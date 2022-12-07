b6a24a086f94c3f9e2490eb9db26a21bc2d04616
kovad003 <kovacsdansan@gmail.com>
2022-12-07 15:09:08 +0200

    The spawned GO's direction can be adjusted in the editor
    - added an aiming tool, that can be used to set the direction

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,31 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    /* EXPOSED FIELDS */
     [SerializeField] private GameObject enemy;
-    [SerializeField] [Range(0, 15)] private float _spawnRate = 3.0f;
+    [SerializeField] [Range(0, 15)] private float spawnRate = 3.0f;
     [SerializeField] private GameObject aimingTool;
+    [SerializeField] private float forceAmplifier = 2;
 
-    private float _time;
+    /* HIDDEN FIELDS */
+    private float time;
 
     // Start is called before the first frame update
     void Start()
     {
-        _time = Time.time;
+        time = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (_time + _spawnRate <= Time.time)
+        if (time + spawnRate <= Time.time)
         {
-            Debug.Log("Enemy spawned!");
             var spawner = transform;
-            Vector2 direction = new Vector2(50, 50);
+            Vector2 direction =  (aimingTool.transform.position - spawner.transform.position).normalized;
             var obj = Instantiate(enemy, spawner.position, spawner.rotation, spawner);
             obj.SetActive(true);
-            obj.GetComponent<Rigidbody2D>().AddForce(direction, ForceMode2D.Impulse);
-            _time = Time.time;
+            obj.GetComponent<Rigidbody2D>().AddForce(forceAmplifier * direction, ForceMode2D.Impulse);
+            
+            time = Time.time;
         }
     }
     
