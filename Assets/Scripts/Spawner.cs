7a19b22871aeaae4cb33dd8228ccc96657ed06a2
kovad003 <kovacsdansan@gmail.com>
2022-12-07 15:22:13 +0200

    The spawner can only create a limited amount of enemies
     - added a counter and a max value.
     - if limit is reached spawner will stop working

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,33 +6,39 @@ public class Spawner : MonoBehaviour
 {
     /* EXPOSED FIELDS */
     [SerializeField] private GameObject enemy;
+    [SerializeField] [Range(0,20)]private int max = 15;
     [SerializeField] [Range(0, 15)] private float spawnRate = 3.0f;
     [SerializeField] private GameObject aimingTool;
     [SerializeField] private float forceAmplifier = 2;
 
     /* HIDDEN FIELDS */
     private float time;
+    private int counter;
 
-    // Start is called before the first frame update
     void Start()
     {
         time = Time.time;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        CreateEnemy();
+    }
+
+    private void CreateEnemy()
+    {
+        if (counter == max) return;
+        
         if (time + spawnRate <= Time.time)
         {
             var spawner = transform;
-            Vector2 direction =  (aimingTool.transform.position - spawner.transform.position).normalized;
+            Vector2 direction = (aimingTool.transform.position - spawner.transform.position).normalized;
             var obj = Instantiate(enemy, spawner.position, spawner.rotation, spawner);
             obj.SetActive(true);
             obj.GetComponent<Rigidbody2D>().AddForce(forceAmplifier * direction, ForceMode2D.Impulse);
-            
+
             time = Time.time;
+            counter++;
         }
     }
-    
-    
 }
