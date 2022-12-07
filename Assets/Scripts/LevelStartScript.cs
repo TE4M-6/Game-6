a182bec1ba303988fe04955b469a4cc20df104e2
Sipuli69 <toni20113@student.hamk.fi>
2022-12-07 15:29:20 +0200

    Blood added, weapon pick up at the start

---
+++ Assets/Scripts/LevelStartScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStartScript : MonoBehaviour
+{
+    [SerializeField] private GameObject playerWeapon;
+    [SerializeField] private GameObject playerCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerWeapon.SetActive(false);
+        playerCharacter.GetComponent<PlayerAiming>().enabled = false;
+        playerCharacter.GetComponent<PlayerShooting>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == playerCharacter)
+        {
+            playerWeapon.SetActive(true);
+            playerCharacter.GetComponent<PlayerAiming>().enabled = true;
+            playerCharacter.GetComponent<PlayerShooting>().enabled = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,6 +9,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+    [SerializeField] private GameObject bloodSplatter;
+
     /* EXPOSED FIELDS: */
     public float _speed = 10f;
     public float _damage = 1f;
@@ -16,10 +18,13 @@ public class Projectile : MonoBehaviour {
         if(!collision.gameObject.CompareTag("Player")) {
             Destroy(gameObject);
         }
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
+
+        // Added by Joona H. - 01122022
+        if (collision.gameObject.CompareTag("Enemy")) {
             collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(_damage);
+            Instantiate(bloodSplatter, collision.gameObject.transform.position, Quaternion.identity);
         }
 
     }
+
 }
