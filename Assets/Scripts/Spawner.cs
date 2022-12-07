8f49286867bc5990792c629894c3e0c47b77def9
kovad003 <kovacsdansan@gmail.com>
2022-12-07 16:48:40 +0200

    Added an aiming offset, so each spawned enemy will take a slightly different route

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     /* EXPOSED FIELDS */
+    [Header("ENEMY")]
     [SerializeField] private GameObject enemy;
     [SerializeField] [Range(0,20)]private int max = 15;
     [SerializeField] [Range(0, 15)] private float spawnCooldown = 3.0f;
+    
+    [Header("AIMING")]
     [SerializeField] private GameObject aimingTool;
+    [SerializeField] [Range(-0.5f, 0.5f)] float aimingOffsetMin = -0.3f;
+    [SerializeField] [Range(-0.5f, 0.5f)] float aimingOffsetMax = -0.3f;
     [SerializeField] private float forceAmplifier = 2;
 
     /* HIDDEN FIELDS */
@@ -39,7 +44,12 @@ public class Spawner : MonoBehaviour
         if (time + spawnCooldown <= Time.time)
         {
             var spawner = transform;
+            float offsetX = Random.Range(aimingOffsetMin, aimingOffsetMax);
+            float offsetY = Random.Range(aimingOffsetMin, aimingOffsetMax);
             Vector2 direction = (aimingTool.transform.position - spawner.transform.position).normalized;
+            direction = new Vector2(direction.x + offsetX, direction.y + offsetY);
+            Debug.Log(direction);
+            
             var obj = Instantiate(enemy, spawner.position, spawner.rotation, spawner);
             obj.SetActive(true);
             obj.GetComponent<Rigidbody2D>().AddForce(forceAmplifier * direction, ForceMode2D.Impulse);
