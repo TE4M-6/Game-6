d11b367e8fdef385e89721f09c34dd7e36d6b791
kovad003 <kovacsdansan@gmail.com>
2022-12-12 13:24:42 +0200

    Improved timer and added it to the lvl completed UI

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,9 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// AUTHOR: @Daniel K.
+/// Last modified: 12 Dec. 2022 by @Daniel K.
+/// </summary>
+/// 
 public class Timer : MonoBehaviour
 {
+    /* EXPOSED FIELDS */
+    [SerializeField] private TextMeshProUGUI timeText;
+    
+    /* HIDDEN FIELDS */
     private static float timer;
     private static bool isTimeMeasured = true;
     
@@ -16,6 +24,12 @@ public class Timer : MonoBehaviour
     void FixedUpdate()
     {
         SampleTime();
+        DisplayTime();
+    }
+
+    private void DisplayTime()
+    {
+        timeText.text = GetTimer();
     }
 
     public static void TurnOffTimer()
@@ -23,10 +37,13 @@ public class Timer : MonoBehaviour
         isTimeMeasured = false;
     }
 
-    public static float GetTimer()
+    public static string GetTimer()
     {
-        Debug.Log("Timer -> " + timer);
-        return timer;
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        int mSeconds = Mathf.FloorToInt(timer*100.0f % 100.0f);
+        string formattedTime = $"{minutes:0}:{seconds:00}:{mSeconds:00}";
+        return formattedTime;
     }
     
     private void SampleTime()
@@ -34,8 +51,5 @@ public class Timer : MonoBehaviour
         if (!isTimeMeasured) return;
         // timer = Time.time;
         timer = Time.timeSinceLevelLoad;
-        Debug.Log(timer);
     }
-    
-    
 }
